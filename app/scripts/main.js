48cc35b04ecca20772106c91c8a2bd1a02c2f3f4
T Jay Carter <tjaycarter@T-Jay-Carters-Mac-Pro.local>
2015-11-28 12:36:15 -0700

    bulk of html starting on styling
    finished html of homepage. working on basic styling now

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,4 +2,8 @@ console.log('\'Allo \'Allo!');
 
 $(function (){
   console.log("Document is ready for action");
+
+  $('.iconContainer').imagefill();
+  $('.logoBox').imagefill(); 
+// image stretches to fill container
 });
