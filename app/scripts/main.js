35b68633a24c105d764103d5874a9f9b1926c32e
T Jay Carter <tjaycarter@T-Jay-Carters-Mac-Pro.local>
2015-11-28 19:16:47 -0700

    fixed footer. on to jQuery

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,7 +4,9 @@ $(function (){
   console.log("Document is ready for action");
 
   $('.imageContainer').imagefill();
-// image stretches to fill container
+  // image stretches to fill container
 
+  //hide the rollover states
+  $('.over').hide();
 
 });
