81d33d03a0d8fc20f2d23428bfb1a451438aa597
T Jay Carter <tjaycarter@T-Jay-Carters-Mac-Pro.local>
2015-11-29 19:52:54 -0700

    coding html and css of laptop-checkout

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,8 +2,11 @@
 'use strict';
 
 $(function (){
-    // image stretches to fill container
+  // image stretches to fill container
   $('.imageContainer').imagefill();
+
+  // breadcrumbs plugin
+  $("#breadcrumbs").rcrumbs();
 
   //hide the rollover states
   //$('#computerOver').hide();
